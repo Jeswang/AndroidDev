f06a4de3abf29eab5f79d8dc8588804b50407e52
xinyu <zhangxinyu019@gmail.com>
2017-05-12 22:12:39 -0700

    uncomplete add image

--- Notes/app/src/main/java/com/example/ldy/notes/NewActivity.java
+++ Notes/app/src/main/java/com/example/ldy/notes/NewActivity.java
@@ -1,7 +1,10 @@
 package com.example.ldy.notes;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +13,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,6 +22,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Observable;
+
+import me.iwf.photopicker.PhotoPicker;
+import rx.Subscriber;
+import rx.Subscription;
 
 public class NewActivity extends AppCompatActivity {
 
@@ -31,6 +48,14 @@ public class NewActivity extends AppCompatActivity {
     private DatabaseReference ref;
 
     private String key;
+
+    private FirebaseStorage mStorage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = mStorage.getReference();
+
+    private ProgressDialog loadingDialog;
+    private ProgressDialog insertDialog;
+    private Subscription subsLoading;
+    private Subscription subsInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,6 +141,19 @@ public class NewActivity extends AppCompatActivity {
                 }
                 break;
             }
+            case (RESULT_OK): {
+                if (resultCode == RESULT_OK) {
+                    if (data != null) {
+                        if (requestCode == 1){
+                            //处理调用系统图库
+                            insertImagesSync(data);
+                        } else if (requestCode == PhotoPicker.REQUEST_CODE){
+                            //异步方式插入图片
+                            insertImagesSync(data);
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -146,5 +184,93 @@ public class NewActivity extends AppCompatActivity {
         newNoteRef.setValue(newNote);
         newNote.setKey(newNoteRef.getKey());
         finish();
+    }
+
+    public void uploadImg(View view) {
+        PhotoPicker.builder()
+                .setPhotoCount(5)//可选择图片数量
+                .setShowCamera(true)//是否显示拍照按钮
+                .setShowGif(true)//是否显示动态图
+                .setPreviewEnabled(true)//是否可以预览
+                .start(this, PhotoPicker.REQUEST_CODE);
+    }
+    private void insertImagesSync(final Intent data){
+        insertDialog.show();
+
+        subsInsert = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                try{
+                    et_new_content.measure(0, 0);
+                    int width = ScreenUtils.getScreenWidth(NewActivity.this);
+                    int height = ScreenUtils.getScreenHeight(NewActivity.this);
+                    ArrayList<String> photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+                    //可以同时插入多张图片
+                    for (String imagePath : photos) {
+                        //Log.i("NewActivity", "###path=" + imagePath);
+                        Bitmap bitmap = ImageUtils.getSmallBitmap(imagePath, width, height);//压缩图片
+                        //bitmap = BitmapFactory.decodeFile(imagePath);
+                        imagePath = SDCardUtil.saveToSdCard(bitmap);
+                        //Log.i("NewActivity", "###imagePath="+imagePath);
+
+                        final String[] downloadUriPath = new String[1];
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+                        byte[] data = baos.toByteArray();
+
+                        HashFunction hashFunction = Hashing.farmHashFingerprint64();
+                        HashCode hashCode = hashFunction.newHasher().putString(baos.toString(), Charset.defaultCharset()).hash();
+                        String filename = hashCode.toString();
+
+                        UploadTask uploadTask = storageRef.child(filename).putBytes(data);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                            }
+                        });
+
+                        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                                downloadUriPath[0] = taskSnapshot.getDownloadUrl().toString();
+                            }
+                        });
+
+                        UploadTask.TaskSnapshot result = com.google.android.gms.tasks.Tasks.await(uploadTask);
+                        downloadUriPath[0] = result.getDownloadUrl().toString();
+
+                        subscriber.onNext(downloadUriPath[0]);
+                    }
+                    subscriber.onCompleted();
+                }catch (Exception e){
+                    e.printStackTrace();
+                    subscriber.onError(e);
+                }
+            }
+        })
+                .onBackpressureBuffer()
+                .subscribeOn(Schedulers.io())//生产事件在io
+                .observeOn(AndroidSchedulers.mainThread())//消费事件在UI线程
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onCompleted() {
+                        insertDialog.dismiss();
+                        et_new_content.addEditTextAtIndex(et_new_content.getLastIndex(), " ");
+                        showToast("图片插入成功");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        insertDialog.dismiss();
+                        showToast("图片插入失败:"+e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(String imagePath) {
+                        et_new_content.insertImage(imagePath, et_new_content.getMeasuredWidth());
+                    }
+                });
     }
 }
